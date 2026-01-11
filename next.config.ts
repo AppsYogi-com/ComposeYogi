cbdb71d71d6c5dd5bd6dc90ce9e7d71caba3bea6
Ananda <rahul.vit09@gmail.com>
2026-01-11 23:35:29 +0530

    feedback widget from UserHero

--- next.config.ts
+++ next.config.ts
@@ -27,7 +27,7 @@ const nextConfig: NextConfig = {
                     },
                     {
                         key: 'Cross-Origin-Embedder-Policy',
-                        value: 'require-corp',
+                        value: 'credentialless',
                     },
                 ],
             },
